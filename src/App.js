d8e1e0816dd4784b7b8de92e808365358e22345f
Natalie <you@example.com>
2022-04-06 18:37:08 -0700

    4/6/22 class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+import { Route, Routes, Link } from 'react-router-dom'
+import CartPage from './cart/CartPage'
+import { SHOPPING_GOODS, CART_ITEMS } from './data'
+import HomePage from './home/HomePage'
+import ShoppingGoodPage from './shopping/ShoppingGoodPage'
+import ShoppingPage from './shopping/ShoppingPage'
 
-function App() {
+
+export default function App() {
+
+  const [shoppingGoods, setShoppingGoods] = useState(SHOPPING_GOODS);
+  const [cartItems, setCartItems] = useState(CART_ITEMS);
+
+  const handleAddToCart = (goodId) => {
+    // look through the cart, see if it's already in the shopping cart
+    const item = cartItems.find( item => item.goodId === goodId )
+    if(item) {
+      setCartItems(currCartItems => {
+        const currItem = currCartItems.find( i => i.goodId === goodId )
+        const index = currCartItems.findIndex( i => i.goodId === goodId )
+
+        const cartItemsCopy = [...currCartItems];
+        const itemCopy = {...currItem, quantity: currItem.quantity + 1 }
+        cartItemsCopy[index] = itemCopy;
+
+        return cartItemsCopy;
+      })
+    }
+    else {
+      // make a new item
+      // if it's not, then we need a new cart item, set to the quanitity 1, add it to the array of car items
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar bg="dark" variant="dark">
+        <div className="container">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/shopping">Shopping</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shopping" element={<ShoppingPage shoppingGoods={shoppingGoods} />} />
+          <Route path="/shopping/:goodId" element={<ShoppingGoodPage shoppingGoods={shoppingGoods} onAddToCart={handleAddToCart} />} />
+          <Route path="/cart" element={<CartPage shoppingGoods={shoppingGoods} cartItems={cartItems} />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+
+export const SHOPPING_GOODS = [
+    {
+        id: 0,
+        name: "Chair",
+        description: "Lorem ipsum dolor sit amet",
+        price: 20
+    },
+    {
+        id: 1,
+        name: "Sofa",
+        description: "It's great",
+        price: 500
+    }
+]
+
+export const CART_ITEMS = [
+    {
+        id: 0,
+        goodId: 1,
+        quantity: 1
+    },
+    {
+        id: 1,
+        goodId: 0,
+        quantity: 2
+    }
+]
